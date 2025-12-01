29081a1a77b31e02e58e5251adc3fa88aae1d077
mbuzz <dev@multibuzz.io>
2025-12-01 20:57:52 +1100

    fix(routes): resolve docs route name conflict

--- config/routes.rb
+++ config/routes.rb
@@ -36,7 +36,7 @@ Rails.application.routes.draw do
   get "signup", to: redirect("/waitlist/new")
 
   # Documentation routes
-  get "docs", to: redirect("/docs/getting-started")
+  get "docs", to: redirect("/docs/getting-started"), as: :docs_index
   get "docs/:page", to: "docs#show", as: :docs, constraints: { page: /[\w-]+/ }
 
   # Dashboard routes
