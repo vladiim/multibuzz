7350cedd6126436afd0cb7bb7c59519846f463c8
mbuzz <ops@mbuzz.co>
2025-12-04 10:38:54 +0000

    feat(email): enable Postmark in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,8 +31,12 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # Postmark for transactional emails (same as production)
+  config.action_mailer.delivery_method = :postmark
+  config.action_mailer.postmark_settings = {
+    api_token: Rails.application.credentials.dig(:postmark, :api_token)
+  }
+  config.action_mailer.raise_delivery_errors = true
 
   # Make template changes take effect immediately.
   config.action_mailer.perform_caching = false
