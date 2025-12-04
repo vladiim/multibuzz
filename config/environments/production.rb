d51716c87bf328eb800249703dc1629a0161d5eb
Vlad Mehakovic <vladiim@users.noreply.github.com>
2025-12-04 12:35:40 +0000

    fix(email): update domain from multibuzz.io to mbuzz.co
    - Fix ApplicationMailer from address to hello@mbuzz.co
    - Fix production mailer URL options to use mbuzz.co

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
     api_token: Rails.application.credentials.dig(:postmark, :api_token)
   }
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: "multibuzz.io" }
+  config.action_mailer.default_url_options = { host: "mbuzz.co" }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
