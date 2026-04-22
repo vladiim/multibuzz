35a805470072289a2c78e2c00eba939dbfa778ac
mbuzz <ops@mbuzz.co>
2026-04-22 16:38:50 +1000

    Bug error email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,8 +90,8 @@ Rails.application.configure do
   # Configure Solid Errors
   config.solid_errors.connects_to = { database: { writing: :errors } }
   config.solid_errors.send_emails = true
-  config.solid_errors.email_from = ""
-  config.solid_errors.email_to = ""
+  config.solid_errors.email_from = "hello@mbuzz.co"
+  config.solid_errors.email_to = "vlad@mbuzz.co"
   config.solid_errors.username = Rails.application.credentials.dig(:solid_errors, :username)
   config.solid_errors.password = Rails.application.credentials.dig(:solid_errors, :password)
 end
