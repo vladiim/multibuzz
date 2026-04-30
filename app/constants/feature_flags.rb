53f7c4e8b5d1824cd06cd12f575647e66e6b616d
mbuzz <ops@mbuzz.co>
2026-04-30 20:14:00 +1000

    feat(feature-flags): add FeatureFlags constants module for known flag names

---
+++ app/constants/feature_flags.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module FeatureFlags
+  META_ADS_INTEGRATION = "meta_ads_integration"
+  LINKEDIN_ADS_INTEGRATION = "linkedin_ads_integration"
+
+  ALL = [ META_ADS_INTEGRATION, LINKEDIN_ADS_INTEGRATION ].freeze
+end
